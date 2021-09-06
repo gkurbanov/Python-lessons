d7a52eded98794cd32f80e96703077a66d438d3f
gkurbanov <geldierkinovich@gmail.com>
2021-09-06 09:40:28 +0500

    Home work 1

---
+++ Task2.py
@@ -0,0 +1,6 @@
+import time
+
+# Ввод секунд
+user_entered_seconds = int(input('Введите секунды - '))
+
+print(time.strftime("%H:%M:%S", time.gmtime(user_entered_seconds)))
---
+++ Task5.py
@@ -0,0 +1,13 @@
+# Выручка
+gain = int(input('Введите выручку - '))
+
+# Потеря
+loss = int(input('Убыток - '))
+
+if loss > gain:
+    print('Ваше предприятие убыточное')
+else:
+    print('Ваше предпрятие преуспевает')
+    personal_qty = int(input('Введите количество сотрудников - '))
+    salary_per_person = (gain - loss) / personal_qty
+    print(f"Зарплата для каждого сотрудника составляет {'%.2f' % salary_per_person}")
