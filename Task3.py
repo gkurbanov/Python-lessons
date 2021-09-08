fbdb6a235873f583db936ee1b0250e41aed6c16d
gkurbanov <geldierkinovich@gmail.com>
2021-09-08 16:38:05 +0500

    master files

--- Task3.py
+++
@@ -1,7 +0,0 @@
-# Введите число
-
-user_selected_number = input('Введите число - ')
-
-overall_sum = int(user_selected_number) + int(user_selected_number + user_selected_number) + int(user_selected_number + user_selected_number + user_selected_number)
-
-print(f"Результат сложения чисел - {overall_sum}")
